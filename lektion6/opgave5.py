92aefcbebb648e1707df6b932ed3fd40077bd66a
pjdk <pajadk@gmail.com>
2019-03-19 07:27:44 +0100

    opgave 5

--- lektion6/opgave5.py
+++ lektion6/opgave5.py
@@ -1,9 +1,9 @@
 from biblioteket import Materiale, Bog, Film
 
 m = Materiale(2, "min bog", 3, 1, 1989)
-udlaam = m.udlaan(1)
+udlaan = m.udlaan(1)
 bog1 = Bog(2, "min bog", 3, 1, 1989, 560, "lars larsen")
 film1 = Film(45, "die hard 3", 15, 2, 2007, "John wien", 90)
 
 print(bog1.tostring())
-print(udlaam)
+print(udlaan)
